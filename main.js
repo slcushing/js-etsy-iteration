6f374b915379571d9f920254a397c11f752d96a0
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-08 16:24:17 -0400

    #5 Eight Materials

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 const prices = items.map(function(number){
     return (number['price']);
 });
-console.log(prices);
+// console.log(prices);
 
 const totalPrices = function(acc, val) {
     return acc + val
@@ -20,7 +20,7 @@ const totalPrices = function(acc, val) {
 
 const answerIs = (prices.reduce(totalPrices)/prices.length);
 
-console.log(answerIs.toFixed(2));
+console.log("The average price is " + answerIs.toFixed(2));
 
 
 // 2.Show me how to get an array of items that cost between $14.00 and $18.00 USD. Please console.log the answer.
@@ -29,15 +29,25 @@ console.log(answerIs.toFixed(2));
 const newList = items.filter(function(item){
     return item.price < 18 && item.price >= 14; 
 });
-console.log(newList);
+console.log(newList); //STILL NEEDS TO CONSOLE.LOG IN CORRECT FORMAT
 
 
 // 3. Show me how find the item with a "GBP" currency code and print its name and price. Please console.log the one you find.
 
-function isEuro(item) {
+// function isEuro(item) {
+//     return item.currency_code === "GBP";  //this would work BUT it's less dynamic (wouldn't work if there were multiple instances in the array)
+// }
+// console.log(items.find(isEuro));
+
+const GBPItems = items.filter(function(item) {
     return item.currency_code === "GBP";
-}
-console.log(items.find(isEuro));
+});
+
+GBPItems.forEach(function(item) {
+    console.log(`${item.title} costs ${item.price}£.`);
+});
+
+ //CONSOLE LOG IN CORRECT FORMAT ---- use forEach?
 
 
 // 4. Show me how to find which items are made of wood. Please console.log the ones you find.
@@ -45,5 +55,12 @@ console.log(items.find(isEuro));
 const areWood = items.filter(function(element){
     return element.materials.includes("wood");
 });
-console.log(areWood);
+console.log(areWood); //CONSOLE LOG IN CORRECT FORMAT
 
+
+//5. Show me how to find which items are made of eight or more materials. Please console.log the ones you find.
+
+const eightPlus = items.filter(function(item) {
+    return item.materials.length > 8;
+});
+console.log(eightPlus);
